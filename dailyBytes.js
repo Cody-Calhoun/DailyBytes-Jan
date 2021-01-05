d3146d3e3bfa34583373c2d6d6999cffddba3273
Will <w.calhoun@outlook.com>
2021-01-04 20:52:37 -0800

    Jan 4: Equality operators

--- dailyBytes.js
+++ dailyBytes.js
@@ -1,30 +1,37 @@
 // Jan 1: Ternary Operator
 
-condition ? expressionIfTrue : expressionIfFalse
+// condition ? expressionIfTrue : expressionIfFalse
 
-Traditional IF/ELSE
-let year = 2021
-if (year == 2020) {
-    console.log("0 of 5 stars. Do not recommend.");
-}
-else {
-    console.log("New year, new me.");
-}
+// Traditional IF/ELSE
+// let year = 2021
+// if (year == 2020) {
+//     console.log("0 of 5 stars. Do not recommend.");
+// }
+// else {
+//     console.log("New year, new me.");
+// }
 
 // Ternary Operator
-let newYear = 2021
-                //  condition  ?    expressionIfTrue               :  expressionIfFalse;
-let yearCheck = (newYear == 2020) ? "0 of 5 stars. Do not recommend." : "New year, new me.";
-console.log(yearCheck);
+// let newYear = 2021
+//  condition  ?    expressionIfTrue               :  expressionIfFalse;
+// let yearCheck = (newYear == 2020) ? "0 of 5 stars. Do not recommend." : "New year, new me.";
+// console.log(yearCheck);
 
 // Jan 2: .push() function
 
-let test = [1, 3, 5, 3, 6];
-console.log(test.push(7));
-console.log(test);
+// let test = [1, 3, 5, 3, 6];
+// console.log(test.push(7));
+// console.log(test);
 
-// Jan. 3: .pop() function
+// Jan 3: .pop() function
 
-let example = [2,4,5,3,6,7]
-console.log(example.pop())
-console.log(example);
+// let example = [2,4,5,3,6,7]
+// console.log(example.pop())
+// console.log(example);
+
+// Jan 4: == vs ===
+
+const year = "2021";
+year == 2021
+  ? console.log("New year new me")
+  : console.log("muct be strictly typed");
